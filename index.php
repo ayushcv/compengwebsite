f8b5dc8c8aed4683d161033acb379be444c13da2
Ayush Singh <74948252+ayushcv@users.noreply.github.com>
2020-12-08 12:46:06 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
-<?php
-    $myfilename = "database.txt";
-    if(file_exists($myfilename)){
-      echo file_get_contents($myfilename);
-    }
+<?php   
+    $path="/home/pi/Resources12/database";
+    $file="database";
+
+    //read file contents
+    $content="
+        <h2>$file</h2>
+            <code>
+                <pre>".htmlspecialchars(file_get_contents("$path/$file"))."</pre>
+            </code>";
+
+    //display
+    echo $content;
 ?>
